6393f034af7725a801d1df2f4224996a828006d3
Charles Fisher <charles.fisher@americaneagle.com>
2022-03-02 16:33:25 -0600

    create an enum for Man United report

--- build/index.js
+++ build/index.js
@@ -1,3 +1,26 @@
 "use strict";
-console.log('hello world');
-console.log('try again');
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs_1 = require("fs");
+const matches = (0, fs_1.readFileSync)('football.csv', {
+    encoding: 'utf-8'
+}).split('\n')
+    .map((row) => {
+    return row.split(',');
+});
+//enum -- enumeration
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["HomeWin"] = "H";
+    MatchResult["AwayWin"] = "A";
+    MatchResult["Draw"] = "D";
+})(MatchResult || (MatchResult = {}));
+let manUnitedWins = 0;
+for (let match of matches) {
+    if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+        manUnitedWins++;
+    }
+    else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+        manUnitedWins++;
+    }
+}
+console.log(`Man United won ${manUnitedWins} games`);
