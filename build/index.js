9c0b47e3ae998bb7976ab7afb4f8cca44a1adc64
Charles Fisher <charles.fisher@americaneagle.com>
2022-03-04 13:33:53 -0600

    add run method to wins analysis class

--- build/index.js
+++ build/index.js
@@ -1,20 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const MatchReader_1 = require("./MatchReader");
-const MatchResult_1 = require("./MatchResult");
 const CsvFileReader_1 = require("./CsvFileReader");
 //Create an object that satisfies the data reader interface
 const footballReader = new CsvFileReader_1.CsvFileReader('football.csv');
 //Create an instance of MatchReader and pass in something satisfying the datareader interface
 const matchReader = new MatchReader_1.MatchReader(footballReader);
 matchReader.load();
-let manUnitedWins = 0;
-for (let match of matchReader.matches) {
-    if (match[1] === 'Man United' && match[5] === MatchResult_1.MatchResult.HomeWin) {
-        manUnitedWins++;
-    }
-    else if (match[2] === 'Man United' && match[5] === MatchResult_1.MatchResult.AwayWin) {
-        manUnitedWins++;
-    }
-}
-console.log(`Man United won ${manUnitedWins} games`);
